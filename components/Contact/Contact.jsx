29b940997f4789ad37265f42fe2d3d0e6fda04f7
alienfacepalm <brandon@alienfacepalm.com>
2017-02-22 03:38:40 -0800

    fix clipboard, move to new lib

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import toastr from 'toastr';
 import '../../node_modules/toastr/build/toastr.min.css';
-import Clipboard from 'clipboard';
+import ClipboardButton from 'react-clipboard.js';
 
 import './contact.less';
 
@@ -10,25 +10,15 @@ const Contact = props => {
 	return (
 		<div className="col-md-12 text-center section">
 			<h1>Contact</h1>
-			<p className="email" 
-			   data-clipboard-action="copy"
-			   data-clipboard-text="brandon.pliska+resume@gmail.com" 
-			   onClick={copyEmail}>brandon.pliska+resume@gmail.com</p>
+			<ClipboardButton className="copy-email" data-clipboard-text="brandon.pliska+resume@gmail.com" onSuccess={copyEmail}>
+		     	<p className="email">brandon.pliska+resume@gmail.com</p>	
+		    </ClipboardButton>
 		</div>
 	);
 };
 
-const copyEmail = props => {
-	let clipboard = new Clipboard('.email');
-	console.log(clipboard);
-	clipboard.on('success', (event) => {
-		console.log('success');
-		toastr.success("My email address has been copied to your clipboard, send me something!");
-	});
-	clipboard.on('error', (error) => {
-		console.log('error');
-	});
-	clipboard.destroy();	
-}
+const copyEmail = event => {
+	toastr.success("My email has been copied to your clipboard. Send me a message!");
+};
 
 export default Contact;
