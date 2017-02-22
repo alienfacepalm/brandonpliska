a41e51bfe2600dd259122fd2722329d1e02b7381
alienfacepalm <brandon@alienfacepalm.com>
2017-02-22 02:47:13 -0800

    add cursor and clipboard

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import toastr from 'toastr';
+import '../../node_modules/toastr/build/toastr.min.css';
+import Clipboard from 'clipboard';
+
+import './contact.less';
+
 const Contact = props => {
 	return (
 		<div className="col-md-12 text-center section">
 			<h1>Contact</h1>
+			<p className="email" 
+			   data-clipboard-action="copy"
+			   data-clipboard-text="brandon.pliska+resume@gmail.com" 
+			   onClick={copyEmail}>brandon.pliska+resume@gmail.com</p>
 		</div>
 	);
+};
+
+const copyEmail = props => {
+	let clipboard = new Clipboard('.email');
+	console.log(clipboard);
+	clipboard.on('success', (event) => {
+		console.log('success');
+		toastr.success("My email address has been copied to your clipboard, send me something!");
+	});
+	clipboard.on('error', (error) => {
+		console.log('error');
+	});
+	clipboard.destroy();	
 }
 
 export default Contact;
--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = props => {
 	return (
 		<div className="col-md-12 text-center section">
 			<h1>Projects</h1>
-			<p class="coming-soon">Coming Soon...</p>
+			<p className="coming-soon">Coming Soon...</p>
 		</div>
 	);
 }
